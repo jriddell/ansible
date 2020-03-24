14223f09fd4bb3de3fccef7041a8c952dbb8c21a
Jonathan Riddell <jr@jriddell.org>
2020-03-24 18:14:28 +0000

    copy gpg stuff over

--- run-setup.sh
+++ run-setup.sh
@@ -4,4 +4,7 @@ set -xe
 
 . variables.inc
 
-ansible-playbook -i inventory -e "pass=${pass}" user.yml
+mv ~/.gnupg/S.uiserver ~/tmp/ || true
+#ansible-playbook -i inventory -e "pass=${pass}" user.yml
+ansible-playbook -i inventory -e "pass=${pass} ansible_sudo_pass=${ansible_sudo_pass}" testing.yml
+mv ~/tmp/S.uiserver ~/.gnupg/ || true
